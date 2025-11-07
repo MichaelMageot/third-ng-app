3f86c4141fd1f35c1b1c14a422f8aef0890fb819
Niihama <michaelmageot@yahoo.fr>
2025-11-07 18:57:28 +0100

    cli & core update

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodosService } from '../services/todos.service';
+import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs/internal/operators/catchError';
+
+@Component({
+  selector: 'app-todos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.scss'
+})
+export class TodosComponent implements OnInit {
+  todoService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([]);
+
+  ngOnInit(): void {
+    this.todoService
+    .getTodosFromApi()
+    .pipe(
+      catchError((err) => {
+        console.log(err);
+        throw err;
+      })
+    )
+    .subscribe((todos) => {
+      this.todoItems.set(todos);
+    });
+  }
+}
